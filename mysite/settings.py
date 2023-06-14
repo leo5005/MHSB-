88358a92e3f2fc93b107272346643b51e7231172
leo <abcyasu5005@gmail.com>
2023-06-14 23:11:16 +0900

    fujitaleo kadai

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE':'django.db.backends.postgresql_psycopg2', # PostgreSQL使いますよ宣言
+        'NAME': 'postgres', # データベース名
+        'USER': 'postgres', # データベースに接続するDBユーザー名
+        'PASSWORD': 'acid5005', # データベースに接続する際のDBユーザのパスワード
+        'HOST': '', # 'localhost'
+        'PORT': '', # 5432
     }
 }
 
